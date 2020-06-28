a60a15d75569429d110e3475d8e7f7dad92f3348
Roselaine Moreira <49179422+Roselaine-Moreira@users.noreply.github.com>
2020-06-28 17:10:38 -0300

    Update GerenciadorRotas.java
    GetInstance e método de leitura das rotas.

--- poo_tf_201/src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ poo_tf_201/src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,14 +1,30 @@
 package pucrs.myflight.modelo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class GerenciadorRotas {
 
     private ArrayList<Rota> rotas;
+    private static GerenciadorRotas instance;
 
-    public GerenciadorRotas() {
+
+    private GerenciadorRotas() {
         this.rotas = new ArrayList<>();
+    }
+
+    public static GerenciadorRotas getInstance() {
+        if (instance == null) {
+            instance = new GerenciadorRotas();
+        }
+        return instance;
     }
 
     public void ordenarCias() {
@@ -16,27 +32,28 @@ public class GerenciadorRotas {
     }
 
     public void ordenarNomesCias() {
-        rotas.sort( (Rota r1, Rota r2) ->
-          r1.getCia().getNome().compareTo(
-          r2.getCia().getNome()));
+        rotas.sort((Rota r1, Rota r2) ->
+                r1.getCia().getNome().compareTo(
+                        r2.getCia().getNome()));
     }
 
     public void ordenarNomesAeroportos() {
-        rotas.sort( (Rota r1, Rota r2) ->
+        rotas.sort((Rota r1, Rota r2) ->
                 r1.getOrigem().getNome().compareTo(
-                r2.getOrigem().getNome()));
+                        r2.getOrigem().getNome()));
     }
 
     public void ordenarNomesAeroportosCias() {
-        rotas.sort( (Rota r1, Rota r2) -> {
-           int result = r1.getOrigem().getNome().compareTo(
-                   r2.getOrigem().getNome());
-           if(result != 0)
-               return result;
-           return r1.getCia().getNome().compareTo(
-                   r2.getCia().getNome());
+        rotas.sort((Rota r1, Rota r2) -> {
+            int result = r1.getOrigem().getNome().compareTo(
+                    r2.getOrigem().getNome());
+            if (result != 0)
+                return result;
+            return r1.getCia().getNome().compareTo(
+                    r2.getCia().getNome());
         });
     }
+
     public void adicionar(Rota r) {
         rotas.add(r);
     }
@@ -44,12 +61,48 @@ public class GerenciadorRotas {
     public ArrayList<Rota> listarTodas() {
         return new ArrayList<>(rotas);
     }
+    public void carregaDados(String nomeArq){
+
+        Path path2 = Paths.get(nomeArq);
+        try (BufferedReader br = Files.newBufferedReader(path2, Charset.defaultCharset()))
+        {
+            String header = br.readLine();
+            String linha = null;
+
+            GerenciadorCias gerenciadorCias = GerenciadorCias.getInstance();
+            GerenciadorAeroportos gerenciadorAeroportos = GerenciadorAeroportos.getInstance();
+            GerenciadorAeronaves gerenciadorAeronaves = GerenciadorAeronaves.getInstance();
+
+            while((linha = br.readLine()) != null) {
+                Scanner sc = new Scanner(linha).useDelimiter(";"); // separador é ;
+                String codCia, codAeroOrigem, codAeroDestino, codeshare, paradas, codAeronave;
+
+                codCia = sc.next();
+                codAeroOrigem = sc.next();
+                codAeroDestino = sc.next();
+                codeshare = sc.next();
+                paradas = sc.next();
+                codAeronave = sc.next();
+
+                Rota rota = new Rota(gerenciadorCias.buscarCodigo(codCia), gerenciadorAeroportos.buscarCodigo(codAeroOrigem),
+                        gerenciadorAeroportos.buscarCodigo(codAeroDestino), gerenciadorAeronaves.buscarCodigo(codAeronave));
+                this.rotas.add(rota);
+            }
+        }
+        catch (IOException x) {
+            System.err.format("Erro na manipulação do arquivo.");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public ArrayList<Rota> buscarOrigem(String codigo) {
         ArrayList<Rota> result = new ArrayList<>();
-        for(Rota r: rotas)
-            if(r.getOrigem().getCodigo().equals(codigo))
+        for (Rota r : rotas)
+            if (r.getOrigem().getCodigo().equals(codigo))
                 result.add(r);
         return result;
     }
 }
+
