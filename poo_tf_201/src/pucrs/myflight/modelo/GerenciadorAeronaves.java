f2a1e0713f89d86265d0baba1ef12cbdd23d8ffa
Roselaine Moreira <49179422+Roselaine-Moreira@users.noreply.github.com>
2020-06-28 17:06:50 -0300

    Update GerenciadorAeronaves.java
    GetInstance e método de leitura das aeronaves.

--- poo_tf_201/src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ poo_tf_201/src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -1,15 +1,30 @@
 package pucrs.myflight.modelo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class GerenciadorAeronaves {
 
     private ArrayList<Aeronave> avioes;
 
-    public GerenciadorAeronaves() {
+    private static GerenciadorAeronaves instance;
+
+    private GerenciadorAeronaves() {
         this.avioes = new ArrayList<>();
+    }
+
+    public static GerenciadorAeronaves getInstance() {
+        if (instance == null) {
+            instance = new GerenciadorAeronaves();
+        }
+        return instance;
     }
 
     public void adicionar(Aeronave aviao) {
@@ -21,35 +36,48 @@ public class GerenciadorAeronaves {
     }
 
     public Aeronave buscarCodigo(String codigo) {
-        for(Aeronave a: avioes)
-            if(a.getCodigo().equals(codigo))
+        for (Aeronave a : avioes)
+            if (a.getCodigo().equals(codigo))
                 return a;
         return null;
     }
 
     public void ordenarDescricao() {
-        // Usando Comparable<Aeronave> em Aeronave
-        //Collections.sort(avioes);
-
-        // Usando expressão lambda
-        //avioes.sort( (Aeronave a1, Aeronave a2) ->
-        //    a1.getDescricao().compareTo(a2.getDescricao()));
-
-        // Mesma coisa, usando método static da interface Comparator:
-        //avioes.sort(Comparator.comparing(a -> a.getDescricao()));
-
-        // Invertendo o critério de comparação com reversed():
         avioes.sort(Comparator.comparing(Aeronave::getDescricao).reversed());
     }
 
+    public void carregaDados(String nomeArq) {
+
+        Path path2 = Paths.get(nomeArq);
+        try (BufferedReader br = Files.newBufferedReader(path2, Charset.defaultCharset())) {
+            String header = br.readLine();
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                Scanner sc = new Scanner(linha).useDelimiter(";"); // separador é ;
+                String codigo, descricao;
+                int capacidade;
+                codigo = sc.next();
+                descricao = sc.next();
+                capacidade = Integer.parseInt(sc.next());
+
+                Aeronave aero = new Aeronave(codigo, descricao, capacidade);
+                this.avioes.add(aero);
+            }
+        } catch (IOException x) {
+            System.err.format("Erro na manipulação do arquivo.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public void ordenarCodigoDescricao() {
-       // Ordenando pelo código e desempatando pela descrição
-       avioes.sort(Comparator.comparing(Aeronave::getCodigo).
-               thenComparing(Aeronave::getDescricao));
+        // Ordenando pelo código e desempatando pela descrição
+        avioes.sort(Comparator.comparing(Aeronave::getCodigo).
+                thenComparing(Aeronave::getDescricao));
     }
 
     public void ordenarCodigo() {
-        avioes.sort( (Aeronave a1, Aeronave a2) ->
-            a1.getCodigo().compareTo(a2.getCodigo()));
+        avioes.sort((Aeronave a1, Aeronave a2) ->
+                a1.getCodigo().compareTo(a2.getCodigo()));
     }
 }
