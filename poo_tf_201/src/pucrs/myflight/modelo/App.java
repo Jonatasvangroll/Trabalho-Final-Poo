9d76cf1d06609e83f66393221544774bef864549
Jonatasvangroll <jonatas.vangroll@gmail.com>
2020-07-01 10:39:33 -0300

    alteração
    correção de bugs

--- poo_tf_201/src/pucrs/myflight/modelo/App.java
+++ poo_tf_201/src/pucrs/myflight/modelo/App.java
@@ -1,6 +1,7 @@
 package pucrs.myflight.modelo;
 
-import pucrs.myflight.modelo.TrafegoAeroporto;
+import pucrs.myflight.modelo.models.TrafegoAeroporto;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -10,28 +11,32 @@ import java.util.ArrayList;
 public class App {
 
 	public static void main(String[] args) {
-        //leitura do arquivo "airlines.dat"
+
+		//TESTE LEITURA DE DADOS DO ARQUIVO EQUIPMENT.DAT
+	    GerenciadorAeronaves gerenciadorAeronaves = new GerenciadorAeronaves();
+        System.out.println(gerenciadorAeronaves.listarTodas().size());
+
+        //TESTE LEITURA DE DADOS DO ARQUIVO AIRLINES.DAT
 		GerenciadorCias gerenciadorCias = new GerenciadorCias();
 		System.out.println(gerenciadorCias.listarTodas().size());
 
-        //leitura do arquivo "equipment.dat"
-        GerenciadorAeronaves gerenciadorAeronaves = new GerenciadorAeronaves();
-        System.out.println(gerenciadorAeronaves.listarTodas().size());
-        
-        //leitura do arquivo "routes.dat"
-        GerenciadorRotas gerenciadorRotas = new GerenciadorRotas();
-        System.out.println(gerenciadorRotas.listarTodas().size());
-        
-        //leitura do arquivo "airports.dat"
+        //TESTE LEITURA DE DADOS DO ARQUIVO COUNTRIES.DAT
+        GerenciadorPaises gerenciadorPaises = new GerenciadorPaises();
+        System.out.println(gerenciadorPaises.listarTodas().size());
+
+        //TESTE LEITURA DE DADOS DO ARQUIVO AIRPORTS.DAT
         GerenciadorAeroportos gerenciadorAeroportos = new GerenciadorAeroportos();
         System.out.println(gerenciadorAeroportos.listarTodos().size());
-        gerenciadorAeroportos.listaAeroportosPorCodCia("2B", gerenciadorRotas);
 
-        //leitura do arquivo "countries.dat"
-        GerenciadorDePaises gerenciadorPaises = new GerenciadorDePaises();
-        System.out.println(gerenciadorPaises.listarTodosPaises().size());
+        //TESTE LEITURA DE DADOS DO ARQUIVO ROUTES.DAT
+        GerenciadorRotas gerenciadorRotas = new GerenciadorRotas();
+        System.out.println(gerenciadorRotas.listarTodas().size());
+
+        gerenciadorAeroportos.listarAeroportosPorCodCompanhia("2B", gerenciadorRotas);
 
         ArrayList<TrafegoAeroporto> ta = gerenciadorAeroportos.estimativaTrafegoPorAeroporto(gerenciadorRotas, "BR");
+
         System.out.println(ta.size());
+
 	}
 }
