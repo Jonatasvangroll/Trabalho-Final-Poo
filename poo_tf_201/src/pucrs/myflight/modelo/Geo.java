e59c81da54536c7369fb306aec939c162e86e633
JessicaFreua <54455967+JessicaFreua@users.noreply.github.com>
2020-06-30 22:08:23 -0300

    Update Geo.java

--- poo_tf_201/src/pucrs/myflight/modelo/Geo.java
+++ poo_tf_201/src/pucrs/myflight/modelo/Geo.java
@@ -5,29 +5,31 @@ import org.jxmapviewer.viewer.GeoPosition;
 // Geo herda de GeoPosition, que é a classe usada internamente
 // pelo desenhador do mapa (não faz sentido recriar tudo novamente...)
 public class Geo extends GeoPosition {
-
+	
 	public Geo(double latitude, double longitude) {
-		super(latitude, longitude);
+		super(latitude, longitude);		
 	}
-
-	//velocidade padrão dos aviões.
-	public final double velocidadeAvioes = 805;
-	//tempo de decolagem e aterrissagem
-	double aterriDec = 30;
-	// Metodo que calcula a distancia entre geolocalizações
-	public double distancia(Geo outraLocalizacao) {
+	public final double VELOCIDADEPADRAO = 805;
+	// Metodo para calcular a distancia entre
+	// calcula a distancia entre geolocalizações
+	public double distancia(Geo outra) {
 		Geo obj = new Geo(getLatitude(), getLongitude());
-		return distancia(obj, outraLocalizacao);
-
+		return distancia(obj, outra);
+		
 	}
 
 	public double tempoViagem(Geo locConexao, Geo locDestino) {
-		double rota1 = this.distancia(locConexao);
-		double rota2 = locConexao.distancia(locDestino);
-		double distancia = rota1 + rota2;
-		return (distancia/velocidadeAvioes + aterriDec);
+		double distRota1 = this.distancia(locConexao);
+		double distRota2 = locConexao.distancia(locDestino);
+		double dist = distRota1 + distRota2;
+		return (dist/VELOCIDADEPADRAO);
 	}
 
+	public double duracaoViagem(Geo geo) {
+		double dist = this.distancia(geo);
+		return (dist/VELOCIDADEPADRAO);
+	}
+	
 	// Metodo de classe (static) para calcular
 	// distancias entre dois objetos Geo informados
 	public static double distancia(Geo geo1, Geo geo2) {
@@ -35,19 +37,19 @@ public class Geo extends GeoPosition {
 		double lat2 = Math.toRadians(geo2.getLatitude());
 		double lon1 = Math.toRadians(geo1.getLongitude());
 		double lon2 = Math.toRadians(geo2.getLongitude());
-
+		
 		double diflat = (lat1-lat2)/2;
 		double diflon = (lon1-lon2)/2;
-
+		
 		double d = Math.pow(Math.sin(diflat),2)+
-				Math.pow(Math.sin(diflon),2)*
-						Math.cos(lat1) * Math.cos(lat2);
-
+				   Math.pow(Math.sin(diflon),2)*
+				   Math.cos(lat1) * Math.cos(lat2);
+		
 		d = 2 * 6371 * Math.asin(Math.sqrt(d));
-
-		return d;
-	}
-
+				
+		return d;	
+	}	
+	
 	@Override
 	public String toString() {
 		String lat = ""+getLatitude();
@@ -60,5 +62,5 @@ public class Geo extends GeoPosition {
 		else lon = getLongitude()+" E";
 		return "("+lat+", "+lon+")";
 	}
-
+	
 }
